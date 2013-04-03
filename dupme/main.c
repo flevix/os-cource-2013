2da6833fdd90572ddb3f72c643c8b1cfbb2392f3
flevix <flevix.ru@gmail.com>
2013-04-03 18:43:20 +0400

    fourth

--- dupme/main.c
+++ dupme/main.c
@@ -1,10 +1,38 @@
 #include <unistd.h>
+#include <stdlib.h>
+
+int str2int(char *source)
+{
+    int result = 0;
+    while (*source != 0)
+    {
+        result = 10 * result + (*source - '0');
+        source++;
+    }
+    return result;
+}
 
 int main(int argc, char** argv)
 {
     if (argc != 2)
     {
+        write(2, "<Incorrect command line argument>\n", 35);
         return 1;
     }
+    int k = str2int(argv[1]);
+    if (k < 1)
+    {
+        write(2, "<Non positive argument>\n",25);
+        return 1;
+    }
+    char *input = malloc(k * sizeof(char));
+    char *output = malloc(k * sizeof(char));
+    while (0)
+    {
+        break;
+    }
+    write(1, "Good!\n", 7);
+    free(input);
+    free(output);
     return 0;
 }
