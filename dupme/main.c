565e3b70a0ebc0d5466a4c1479f8d48b6469aa77
flevix <flevix.ru@gmail.com>
2013-04-04 01:05:38 +0400

    sixth commit

--- dupme/main.c
+++ dupme/main.c
@@ -6,10 +6,35 @@ int str2int(char *source)
     int result = 0;
     while (*source != 0)
     {
+        if (*source < '0' || *source > '9')
+        {
+            write(2, "<Incorrect argument>\n", 22);
+            return 1;
+        }
+        if (result > (2147483647 - 9) / 10) {
+            write(2, "<Argument larger then maximum allowable value>\n", 48);
+            return 1;
+        }
         result = 10 * result + (*source - '0');
         source++;
     }
     return result;
+}
+
+char *output, *input;
+
+int print(int length)
+{
+    int write_count = 0;
+    while (length > 0)
+    {
+        write_count = write(1, input + write_count, length);
+        if (write_count < 0) {
+            write(2, "<Writing error>\n", 17);
+            return 1;
+        }
+        length -= write_count;
+    }
 }
 
 enum state
@@ -27,15 +52,16 @@ int main(int argc, char** argv)
     int k = str2int(argv[1]);
     if (k < 1)
     {
-        write(2, "<Non positive argument>\n",25);
+        write(2, "<Non positive argument>\n", 25);
         return 1;
     }
-    char *input = malloc(k * sizeof(char));
-    char *output = malloc(k * sizeof(char));
+    input = malloc(k * sizeof(char));
+    output = malloc(k * sizeof(char));
 
     last_case = NORMAL;
     int length = 0;
     int read_count = 0;
+    int end_of_file = 0;
     while (0)
     {
         while (length != k)
@@ -48,12 +74,37 @@ int main(int argc, char** argv)
             }
             if (read_count == 0) {
                 //some code
+                write(1, "<Very good!>", 12);
+                end_of_file = 1;
+                break;
             }
             length += read_count;
+        }
+        if (end_of_file = 1)
+        {
+            //some code
+            break;
         }
         switch (last_case)
         {
             case NORMAL:
+                last_case = PENDING;
+                for (int i = 0; i < k; i++)
+                {
+                    if (input[i] == '\n')
+                    {
+                        memcpy(output, input, i);
+                        print(i);
+                        print(i);
+                        memmove(input, input + i + 1, k - i);
+                        last_case = NORMAL;
+                        break;
+                    }
+                }
+                if (last_case == PENDING)
+                {
+                    memcpy(output, input, k);
+                }
                 break;
             case IGNORING:
                 for (int i = 0; i < k; i++) 
@@ -72,7 +123,8 @@ int main(int argc, char** argv)
                 {
                     last_case = NORMAL;
                     length = k - 1;
-                    //write
+                    print(k);
+                    print(k);
                 } else {
                     last_case = IGNORING;
                 }
