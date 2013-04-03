471d60566ec2a0e04b7d1db95d33f0154d2b06c8
flevix <flevix.ru@gmail.com>
2013-04-04 00:13:28 +0400

    fifth commit

--- dupme/main.c
+++ dupme/main.c
@@ -12,6 +12,11 @@ int str2int(char *source)
     return result;
 }
 
+enum state
+{
+    NORMAL, IGNORING, PENDING
+} last_case;
+
 int main(int argc, char** argv)
 {
     if (argc != 2)
@@ -27,9 +32,53 @@ int main(int argc, char** argv)
     }
     char *input = malloc(k * sizeof(char));
     char *output = malloc(k * sizeof(char));
+
+    last_case = NORMAL;
+    int length = 0;
+    int read_count = 0;
     while (0)
     {
-        break;
+        while (length != k)
+        {
+            read_count = read(0, input + length, k - length);
+            if (read_count < 0)
+            {
+                write(2, "<Reading error>\n", 17);
+                return 1;
+            }
+            if (read_count == 0) {
+                //some code
+            }
+            length += read_count;
+        }
+        switch (last_case)
+        {
+            case NORMAL:
+                break;
+            case IGNORING:
+                for (int i = 0; i < k; i++) 
+                {
+                    if (input[i] == '\n')
+                    {
+                        last_case = NORMAL;
+                        memmove(input, input + i + 1, k - i);
+                        length = k - i;
+                        break;
+                    }
+                }
+                break;
+            case PENDING:
+                if (input[0] == '\n')
+                {
+                    last_case = NORMAL;
+                    length = k - 1;
+                    //write
+                } else {
+                    last_case = IGNORING;
+                }
+                memmove(input, input + 1, k - 1);
+                break;
+        }
     }
     write(1, "Good!\n", 7);
     free(input);
