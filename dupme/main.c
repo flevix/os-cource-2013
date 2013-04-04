3b94461b726479d2ae636664c9ea764c6f260308
flevix <flevix.ru@gmail.com>
2013-04-04 22:28:51 +0400

    This solution is wrong

--- dupme/main.c
+++ dupme/main.c
@@ -21,22 +21,28 @@ int str2int(char *source)
     return result;
 }
 
-char *output, *input;
+char *data;
 
 int print(int length)
 {
+    
     int write_count = 0;
     while (length > 0)
     {
-        write_count = write(1, output + write_count, length + 1);
+        write_count = write(1, data + write_count, length);
         if (write_count < 0) {
             write(2, "<Writing error>\n", 17);
             exit(1);
         }
         length -= write_count;
     }
+    write(1, "\n", 1);
 }
 
+void exit_error()
+{
+    exit(1);
+}
 enum state
 {
     NORMAL, IGNORING, PENDING
@@ -50,97 +56,51 @@ int main(int argc, char** argv)
         exit(1);
     }
     int k = str2int(argv[1]);
-    input = malloc(k * sizeof(char));
-    output = malloc((k + 1) * sizeof(char));
+    data = malloc(++k * sizeof(char));
 
     last_case = NORMAL;
     int length = 0;
     int read_count = 0;
     int end_of_file = 0;
     int i = 0;
+    int start = 0, end = 0, count = 0;
     while (1)
     {
         while (length != k && !end_of_file)
         {
-            read_count = read(0, input + length, k - length);
+            read_count = read(0, data + length, k - length);
             if (read_count < 0)
-            {
-                write(2, "<Reading error>\n", 17);
-                exit(1);
-            }
-            if (read_count == 0) {
-                //write(1, "<Very good!>", 12);
+                exit_error();
+            if (read_count == 0)
                 end_of_file = 1;
-                break;
-            }
             length += read_count;
         }
-        if (end_of_file == 1)
-        {
-            //write(1, "<EOF>", 5);
-            if (last_case != IGNORING)
-            {
-                //write(1, "<EOF_N>", 7);
-                print(i);
-                print(i);
+        for (i = 0; i < length; i++)
+            if (data[i] == '\n') {
+                if (last_case == IGNORING) {
+                    memmove(data, data + i + 1, k - i);
+                    last_case = NORMAL;
+                    length -= i;
+                    i = 0;
+                } else if (last_case == NORMAL) {
+                    if (i < k) {
+                        print(i);
+                    }
+                    memmove(data, data + i + 1, k - i);
+                    length -= i;
+                    i = 0;
+                }
             }
+        if (end_of_file) {
+            print(i);
             break;
         }
-        switch (last_case)
-        {
-            case NORMAL:
-                last_case = PENDING;
-                i = 0;
-                for ( i = 0; i < k; i++)
-                {
-                    if (input[i] == '\n')
-                    {
-                        memcpy(output, input, i);
-                        output[i] = '\n';
-                        print(i);
-                        print(i);
-                        memmove(input, input + i + 1, k - i);
-                        last_case = NORMAL;
-                        length -= i + 1;
-                        break;
-                    }
-                }
-                if (last_case == PENDING)
-                {
-                    memcpy(output, input, k);
-                    output[k] = '\n';
-                    length = 0;
-                }
-                break;
-            case IGNORING:
-                i = 0;
-                for (i = 0; i < k; i++) 
-                {
-                    if (input[i] == '\n')
-                    {
-                        last_case = NORMAL;
-                        memmove(input, input + i + 1, k - i);
-                        length = k - i - 1;
-                        break;
-                    }
-                }
-                break;
-            case PENDING:
-                if (input[0] == '\n')
-                {
-                    last_case = NORMAL;
-                    print(k);
-                    print(k);
-                } else {
-                    last_case = IGNORING;
-                }
-                length -= 1;
-                memmove(input, input + 1, k - 1);
-                break;
-        }
+        if (length = k)
+            last_case = IGNORING;
+        else
+            memmove(data, data + (k - length), length);
     }
     //write(1, "Good!\n", 7);
-    free(input);
-    free(output);
+    free(data);
     return 0;
 }
