4a8b7f4bad2587e2937874c6308031c2df21c013
flevix <flevix.ru@gmail.com>
2013-04-05 00:32:48 +0400

    Cosmetic changes, some tests added.

--- dupme/main.c
+++ dupme/main.c
@@ -1,99 +1,54 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int str2int(char *source)
-{
+int str2int(char *source) { 
     int result = 0;
-    while (*source != 0)
-    {
-        if (*source < '0' || *source > '9')
-        {
-            write(2, "<Incorrect argument>\n", 22);
-            exit(1);
-        }
-        if (result > (2147483647 - 9) / 10) {
-            write(2, "<Argument larger then maximum allowable value>\n", 48);
-            exit(1);
-        }
+    while (*source != 0) {
+        if ((result > (2147483647 - 9) / 10) || *source < '1' || *source > '9') exit(1);
         result = 10 * result + (*source - '0');
         source++;
     }
     return result;
 }
 
-char *data, *out;
+char *data;
 
-int print(int length)
-{
-    memcpy(out, data, length + 1);
-    memcpy(out + length + 1, data, length + 1);
+int print(int length) {
     int write_count = 0;
-    while (length > 0)
-    {
-        write_count = write(1, out + write_count, 2 * (length + 1));
-        if (write_count < 0) {
-            write(2, "<Writing error>\n", 17);
-            exit(1);
-        }
+    while (length > 0) {
+        write_count = write(1, data + write_count, length + 1);
+        if (write_count < 0) exit(1);
         length -= write_count;
     }
 }
 
-void exit_error()
-{
-    exit(1);
-}
-enum state
-{
-    NORMAL, IGNORING, PENDING
+enum state {
+    NORMAL, IGNORING
 } last_case;
 
-int main(int argc, char** argv)
-{
-    if (argc != 2)
-    {
-        write(2, "<Incorrect command line argument>\n", 35);
-        exit(1);
-    }
+int main(int argc, char** argv) {
+    if (argc != 2) exit(1);
     int k = str2int(argv[1]);
     data = malloc(++k * sizeof(char));
-    out = malloc(k * 2 * sizeof(char));
-
     last_case = NORMAL;
-    int length = 0;
-    int read_count = 0;
-    int end_of_file = 0;
+    int length = 0, read_count = 0, end_of_file = 0;
     int i = 0;
-    while (1)
-    {
-        while (length != k && !end_of_file)
-        {
-            read_count = read(0, data + length, k - length);
-            if (read_count < 0)
-                exit_error();
-            if (read_count == 0)
-                end_of_file = 1;
-            length += read_count;
-        }
+    while (end_of_file == 0) {
+        read_count = read(0, data + length, k - length);
+        if (read_count < 0) exit(1);
+        if (read_count == 0) end_of_file = 1;
+        length += read_count;
         for (i = 0; i < length; i++)
             if (data[i] == '\n') {
-                if (last_case == IGNORING) {
-                    memmove(data, data + i + 1, k - i - 1);
-                    last_case = NORMAL;
-                    length -= i + 1;
-                    i = 0;
-                } else if (last_case == NORMAL) {
-                    if (i < k) {
-                        print(i);
-                    }
-                    memmove(data, data + i + 1, k - i - 1);
-                    length -= i + 1;
-                    i = 0;
-                }
+                if (last_case == IGNORING) last_case = NORMAL;
+                if (last_case == NORMAL && i < k) { print(i); print(i); }
+                memmove(data, data + i + 1, k - i - 1);
+                length -= i + 1;
+                i = 0;
             }
         if (end_of_file) {
             data[length] = '\n';
-            print(length);
+            print(length); print(length);
             break;
         }
         if (length == k) {
@@ -101,7 +56,6 @@ int main(int argc, char** argv)
             length = 0;
         }
     }
-    //write(1, "Good!\n", 7);
     free(data);
     return 0;
 }
