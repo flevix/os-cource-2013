c881cfc621d17627413e27ff72c57da83c9d3338
flevix <flevix.ru@gmail.com>
2013-04-05 01:52:57 +0400

    Bug with \n fixed. Final commit.

--- dupme/main.c
+++ dupme/main.c
@@ -24,15 +24,13 @@ int print(int length) {
 
 enum state {
     NORMAL, IGNORING
-} last_case;
+} last_case = NORMAL;
 
 int main(int argc, char** argv) {
     if (argc != 2) exit(1);
     int k = str2int(argv[1]);
     data = malloc(++k * sizeof(char));
-    last_case = NORMAL;
-    int length = 0, read_count = 0, end_of_file = 0;
-    int i = 0;
+    int length = 0, read_count = 0, end_of_file = 0, i = 0;
     while (end_of_file == 0) {
         read_count = read(0, data + length, k - length);
         if (read_count < 0) exit(1);
@@ -41,17 +39,15 @@ int main(int argc, char** argv) {
         for (i = 0; i < length; i++)
             if (data[i] == '\n') {
                 if (last_case == IGNORING) last_case = NORMAL;
-                if (last_case == NORMAL && i < k) { print(i); print(i); }
+                if (last_case == NORMAL && i < k && data[0] != '\n') { print(i); print(i); }
                 memmove(data, data + i + 1, k - i - 1);
                 length -= i + 1;
                 i = 0;
             }
-        if (end_of_file) {
+        if (end_of_file && data[0] != '\n') {
             data[length] = '\n';
             print(length); print(length);
-            break;
-        }
-        if (length == k) {
+        } else if (length == k) {
             last_case = IGNORING;
             length = 0;
         }
