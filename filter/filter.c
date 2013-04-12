8a49bc29484e78ef58416662b76cc2226e5e8d41
flevix <flevix.ru@gmail.com>
2013-04-13 00:13:21 +0400

    Debug mode on. Some bug fixed

--- filter/filter.c
+++ filter/filter.c
@@ -5,16 +5,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int str2int(char *source) { 
-    int result = 0;
-    while (*source != 0) {
-        if ((result > (2147483647 - 9) / 10) || *source < '0' || *source > '9') exit(1);
-        result = 10 * result + (*source - '0');
-        source++;
-    }
-    return result;
-}
-
 char *data;
 char **com;
 
@@ -26,9 +16,11 @@ void print(int length) {
         length -= write_count;
     }
 }
+
 int len = 0;
-int start(int i) 
+void start(int i) 
 {
+    printf("start\n");//debug
     char *buf = malloc((i + 1) * sizeof(char));
     memmove(buf, data, i);
     com[len - 2] = buf;
@@ -42,17 +34,14 @@ int start(int i)
         dup2(fd, 2);
         execvp(com[0], com);
         close(fd);
-        return 0;
+        //return 0;
     }
     wait(&status);
     if (status == 0)
-        printf("%s\n", com[len - 2]);
+        print(i);
+        //printf("%s\n", com[len - 2]);
     //free(buf);
 }
-
-enum state {
-    NORMAL, IGNORING
-} last_case;
 
 int main(int argc, char** argv) {
     int k = 4 * 1024;
@@ -61,16 +50,20 @@ int main(int argc, char** argv) {
     while ((opt = getopt(argc, argv, "nzb:")) != -1) {
         switch (opt) {
             case 'n':
-                delimiter = '\n';
+                printf("del=n\n");
+                delimiter = '\n';//debug
                 break;
             case 'z':
-                delimiter = 0;
+                delimiter = '\0';
+                printf("del=0\n");//debug
                 break;
             case 'b':
-                k = str2int(optarg);
+                printf("del=b\n");//debug
+                k = atoi(optarg);
                 break;
         }
     }
+    printf("%d %c\n", k, delimiter);//debug
     len = argc - optind;
     com = malloc(len + 2);
     int j;
@@ -79,17 +72,19 @@ int main(int argc, char** argv) {
     }
     len += 2;
     data = malloc(++k * sizeof(char));
-    last_case = NORMAL;
     int length = 0, read_count = 0, end_of_file = 0;
     int i = 0;
     while (end_of_file == 0) {
         read_count = read(0, data + length, k - length);
+        printf("%d--%s\n", read_count, data);
         if (read_count < 0) exit(1);
         if (read_count == 0) end_of_file = 1;
         length += read_count;
         for (i = 0; i < length; i++)
             if (data[i] == delimiter) {
-                if (last_case == NORMAL && i < k && data[0] != delimiter) {
+                printf("data[i]==delimiter\n");
+                if (i < k && data[0] != delimiter) {
+                    printf("i<k\n");//debug
                     start(i);
                 }
                 memmove(data, data + i + 1, k - i - 1);
@@ -98,6 +93,7 @@ int main(int argc, char** argv) {
             }
         if (length == k) 
         {
+            printf("length == k, exit");//debug
             exit(1);
         }
         if (end_of_file && data[0] != delimiter) {
@@ -105,6 +101,9 @@ int main(int argc, char** argv) {
             start(length);
         }
     }
-    //free(data);
+    printf("free data\n");//debug
+    printf("\n", data);
+//    free(data);
+    printf("return 0\n");//debug
     return 0;
 }
