4cb042e33aae70b49a0eaef7909147e8433349e7
flevix <flevix.ru@gmail.com>
2013-04-13 01:30:22 +0400

    Debug code deleted

--- filter/filter.c
+++ filter/filter.c
@@ -7,24 +7,32 @@
 
 char *data;
 char **_argv;
+int len = 0;
+
+void _free() {
+    free(data);
+    free(_argv);
+}
+
+void error_exit() {
+    _free();
+    exit(1);
+}
 
 void print(int length) {
     int write_count = 0;
     while (length > 0) {
         write_count = write(1, data + write_count, length + 1);
-        if (write_count < 0) exit(1);
+        if (write_count < 0) error_exit(1);
         length -= write_count;
     }
 }
 
-int len = 0;
-void start(int i) 
+void _exec(int i) 
 {
-    printf("--start\n");//debug
     char *buf = malloc((i + 1) * sizeof(char));
     memmove(buf, data, i);
     _argv[len - 2] = buf;
-    _argv[len - 1] = 0;
     int pid = fork();
     int status;
     if (pid == 0)
@@ -37,9 +45,7 @@ void start(int i)
     }
     wait(&status);
     if (status == 0) {
-        printf("--status0st\n");//debug
         print(i);
-        printf("--status0en\n");//debug
     }
     free(buf);
 }
@@ -51,40 +57,35 @@ int main(int argc, char** argv) {
     while ((opt = getopt(argc, argv, "nzb:")) != -1) {
         switch (opt) {
             case 'n':
-                printf("--del=n\n");
-                delimiter = '\n';//debug
+                delimiter = '\n';
                 break;
             case 'z':
                 delimiter = '\0';
-                printf("--del=0\n");//debug
                 break;
             case 'b':
-                printf("--del=b\n");//debug
                 k = atoi(optarg);
                 break;
         }
     }
-    printf("--%d--%c\n", k, delimiter);//debug
     len = argc - optind + 2;
     _argv = malloc(len);
     int j;
     for (j = 0; j < len - 2; j++) {
         _argv[j] = argv[j + optind];
     }
+    _argv[len - 1] = 0;
     data = malloc(++k * sizeof(char));
     int length = 0, read_count = 0, end_of_file = 0;
     int i = 0;
     while (end_of_file == 0) {
         read_count = read(0, data + length, k - length);
-        if (read_count < 0) exit(1);
+        if (read_count < 0) error_exit(1);
         if (read_count == 0) end_of_file = 1;
         length += read_count;
         for (i = 0; i < length; i++)
             if (data[i] == delimiter) {
-                printf("--data[i]==delimiter\n");//debug
                 if (i < k && data[0] != delimiter) {
-                    printf("--i<k\n");//debug
-                    start(i);
+                    _exec(i);
                 }
                 memmove(data, data + i + 1, k - i - 1);
                 length -= i + 1;
@@ -92,17 +93,13 @@ int main(int argc, char** argv) {
             }
         if (length == k) 
         {
-            printf("--length == k, exit");//debug
-            exit(1);
+            error_exit();
         }
         if (end_of_file && data[0] != delimiter) {
             data[length] = delimiter;
-            start(length);
+            _exec(length);
         }
     }
-    printf("--free data\n");//debug
-    printf("--\n", data);
-    free(data);
-    printf("--return 0\n");//debug
+    _free();
     return 0;
 }
