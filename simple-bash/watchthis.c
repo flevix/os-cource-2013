4a5ebd1a1017ace7b615d769fae80b58f3d31ca8
flevix <flevix.ru@gmail.com>
2013-04-17 18:59:08 +0400

    Rewrite watchthis for more compatibility with watchthis.c + some cosmetic changes in watchthis.c

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -6,7 +6,6 @@
 #include <stdio.h> //printf
 #include <signal.h>
 #include <fcntl.h>
-#include <string.h>
 
 const int MIN_ARGS = 2;
 const int E_BAD_ARGS = 65;
@@ -17,16 +16,24 @@ const char *DIFF="Difference with the previous output --";
 char *STATE_FIRST, *STATE_SECOND;
 char *data;
 
+void error_exit(int code) {
+    free(data);
+    unlink(STATE_FIRST);
+    unlink(STATE_SECOND);
+    printf("\n");
+    exit(code);
+}
+
 void _write(src, dst) {
     int read_count, write_count, pos;
     while (1) {
         read_count = read(src, data, data_size);
-        if (read_count < 0) exit(5);
+        if (read_count < 0) error_exit(5);
         if (read_count == 0) break;
         pos = 0;
         while (read_count > 0) {
             write_count = write(dst, data + pos, read_count);
-            if (write_count < 0) exit(6);
+            if (write_count < 0) error_exit(6);
             read_count -= write_count;
             pos += write_count;
         }
@@ -34,29 +41,23 @@ void _write(src, dst) {
 }
 
 void sigint_exit() {
-    free(data);
-    unlink(STATE_FIRST);
-    unlink(STATE_SECOND);
-    write(2, "\n", 1);
-    exit(2);
+    error_exit(130);
 }
 
 int main(int argc, char** argv) {
     if (argc < MIN_ARGS) {
-        //echo -e "Usage: `basename $0` [time in seconds] [command]"
+        printf("%s\n", "Usage: watchthis [time in seconds] [command]");
         exit(E_BAD_ARGS);
     }
-
     int time_for_wait = atoi(argv[1]);
-    //int pid = getpid(); //TODO filenames+pid
-    
+
     STATE_FIRST = "/tmp/watchthis1.84";
     STATE_SECOND = "/tmp/watchthis2.84";
 
     int fsds[2];
     fsds[0] = open(STATE_FIRST, O_CREAT | O_WRONLY, 0600);
     fsds[1] = open(STATE_SECOND, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fsds[0] < 0 || fsds[1] < 0) exit(2);
+    if (fsds[0] < 0 || fsds[1] < 0) error_exit(2);
     close(fsds[0]);
     close(fsds[1]);
     
@@ -67,7 +68,7 @@ int main(int argc, char** argv) {
         int parent = fork();
         if (parent) {
             wait(&status);
-            if (!WIFEXITED(status) && WEXITSTATUS(status) != 0) exit(4);
+            if (!WIFEXITED(status) && WEXITSTATUS(status) != 0) error_exit(4);
             printf("%s\n", CURR_OUTPUT);
             fsds[0] = open(STATE_FIRST, O_RDONLY);
             _write(fsds[0], 1);
@@ -76,12 +77,12 @@ int main(int argc, char** argv) {
             parent = fork();
             if (parent) {
                 wait(&status);
-                if (!WIFEXITED(status) && WEXITSTATUS(status) != 0) exit(7);
+                if (!WIFEXITED(status) && WEXITSTATUS(status) != 0) error_exit(7);
                 printf("%s\n", DELIMITER);
             } else {
                 printf("%s\n", DIFF);
                 execlp("diff", "diff", "-u", STATE_FIRST, STATE_SECOND, NULL);
-                exit(8);
+                error_exit(8);
             }
         } else {
             fsds[1] = open(STATE_FIRST, O_WRONLY);
@@ -89,7 +90,7 @@ int main(int argc, char** argv) {
             close(fsds[0]);
             close(fsds[1]);
             execvp(argv[2], &argv[2]);
-            exit(255);
+            error_exit(255);
         }
         fsds[0] = open(STATE_FIRST, O_RDONLY);
         fsds[1] = open(STATE_SECOND, O_WRONLY | O_TRUNC);
@@ -98,8 +99,9 @@ int main(int argc, char** argv) {
         close(fsds[1]);
         sleep(time_for_wait);
     }
-    free(data);
-    unlink(STATE_FIRST);
-    unlink(STATE_SECOND);
+    //redundant lines
+    //free(data);
+    //unlink(STATE_FIRST);
+    //unlink(STATE_SECOND);
     return 0;
 }
