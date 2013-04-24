96a79a76a908bde60fc42af037602ca625be323d
flevix <flevix.ru@gmail.com>
2013-04-25 00:34:01 +0400

    THE DEATH OF THE VILE. THE COFFIN, THE COFFIN CEMETERY DEATH.

--- midterm/midterm.c
+++ midterm/midterm.c
@@ -5,8 +5,7 @@
 #include <stdio.h> //printf
 
 const int ARGS_COUNT = 1 + 2;
-int fds[2];
-
+int fds;
 typedef struct {
     int fd;
     int capacity; //size of allocated storage capacity
@@ -17,26 +16,24 @@ typedef struct {
 } STREAM;
 
 STREAM* stream1;
-//STREAM* stream2;
 
 STREAM* init_stream(int fd, int capacity, char delimiter) {
     STREAM *t = (STREAM*) malloc(sizeof(STREAM));
+    if (t == NULL) exit(4);
     t->fd = fd;
     t->capacity = capacity;
     t->size = 0;
     t->delimiter = delimiter;
-    t->data = (char*) malloc(capacity * sizeof(char));
     t->close = 0;
+    t->data = (char*) malloc(capacity);
+    if (t->data == NULL) exit(5);
     return t;
 }
 
 void good_exit() {
-    free(stream1);
     free(stream1->data);
-    //free(stream2);
-    //free(stream2->data);
-    close(fds[0]);
-    //close(fds[1]);
+    free(stream1);
+    close(fds);
 }
 
 void err_exit(int code) {
@@ -45,23 +42,22 @@ void err_exit(int code) {
 }
 
 char* next_token(STREAM* stream) {
+    char* pos = memchr(stream->data, stream->delimiter, stream->size);
     int read_count;
-    while (!stream->close) {
+    while (pos == NULL && (!stream->close)) {
         read_count = read(stream->fd, stream->data + stream->size, stream->capacity - stream->size);
         if (read_count < 0) err_exit(4);
         stream->size += read_count;
         if (read_count == 0) stream->close = 1;
-        if (memchr(stream->data, stream->delimiter, stream->size) == NULL) 
-            continue;
-        else break;
+        pos = memchr(stream->data, stream->delimiter, stream->size);
     }
-    char* pos_delimiter = (char*) memchr(stream->data, stream->delimiter, stream->size);
-    if (pos_delimiter == NULL) {
+    if (pos == NULL) {
         if (stream->size == stream->capacity) err_exit(3);
         return NULL;
     }
-    int token_size = pos_delimiter - stream->data + 1;
-    char* token = (char*) malloc(token_size * sizeof(char));
+    int token_size = pos - stream->data + 1;
+    char* token = (char*) malloc(token_size);
+    if (token == NULL) err_exit(6);
     memcpy(token, stream->data, token_size);
     *(token + token_size - 1) = '\0';
     memmove(stream->data, stream->data + token_size, stream->size - token_size);
@@ -70,14 +66,11 @@ char* next_token(STREAM* stream) {
 }
 
 int main(int argc, char** argv) {
-    if (argc != 3) exit(1);
-    fds[0] = open(argv[1], O_RDONLY);
-    //fds[1] = open(argv[2], O_RDONLY);
-    //if (fds[0] < 0 || fds[1] < 0) exit(2);
-    if (fds[0] < 0) exit(2);
+    if (argc != ARGS_COUNT) exit(1);
+    fds = open(argv[1], O_RDONLY);
+    if (fds < 0) exit(2);
     int buf_capacity = atoi(argv[2]); 
-    stream1 = init_stream(fds[0], buf_capacity, '\n');
-    //stream2 = init_stream(fds[1], 128, '\n');
+    stream1 = init_stream(fds, buf_capacity, '\n');
     char* t;
     while ((t = next_token(stream1)) != NULL) {
         printf("%s\n", t);
