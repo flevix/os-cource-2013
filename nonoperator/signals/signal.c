ff12e84a8b99f52da2faa28669dd161f4f63ae0c
flevix <flevix.ru@gmail.com>
2013-05-17 21:57:40 +0400

    some changes in signal

--- nonoperator/signals/signal.c
+++ nonoperator/signals/signal.c
@@ -2,57 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//SIGHUP      1   /* Hangup (POSIX).  */
-//SIGINT      2   /* Interrupt (ANSI).  */
-//SIGQUIT     3   /* Quit (POSIX).  */
-//SIGILL      4   /* Illegal instruction (ANSI).  */
-//SIGTRAP     5   /* Trace trap (POSIX).  */
-//SIGABRT     6   /* Abort (ANSI).  */
-//SIGIOT      6   /* IOT trap (4.2 BSD).  */
-//SIGBUS      7   /* BUS error (4.2 BSD).  */
-//SIGFPE      8   /* Floating-point exception (ANSI).  */
-//SIGKILL     9   /* Kill, unblockable (POSIX).  */
-//SIGUSR1     10  /* User-defined signal 1 (POSIX).  */
-//SIGSEGV     11  /* Segmentation violation (ANSI).  */
-//SIGUSR2     12  /* User-defined signal 2 (POSIX).  */
-//SIGPIPE     13  /* Broken pipe (POSIX).  */
-//SIGALRM     14  /* Alarm clock (POSIX).  */
-//SIGTERM     15  /* Termination (ANSI).  */
-//SIGSTKFLT   16  /* Stack fault.  */
-//SIGCLD      SIGCHLD /* Same as SIGCHLD (System V).  */
-//SIGCHLD     17  /* Child status has changed (POSIX).  */
-//SIGCONT     18  /* Continue (POSIX).  */
-//SIGSTOP     19  /* Stop, unblockable (POSIX).  */
-//SIGTSTP     20  /* Keyboard stop (POSIX).  */
-//SIGTTIN     21  /* Background read from tty (POSIX).  */
-//SIGTTOU     22  /* Background write to tty (POSIX).  */
-//SIGURG      23  /* Urgent condition on socket (4.2 BSD).  */
-//SIGXCPU     24  /* CPU limit exceeded (4.2 BSD).  */
-//SIGXFSZ     25  /* File size limit exceeded (4.2 BSD).  */
-//SIGVTALRM   26  /* Virtual alarm clock (4.2 BSD).  */
-//SIGPROF     27  /* Profiling alarm clock (4.2 BSD).  */
-//SIGWINCH    28  /* Window size change (4.3 BSD, Sun).  */
-//SIGPOLL     SIGIO   /* Pollable event occurred (System V).  */
-//SIGIO       29  /* I/O now possible (4.2 BSD).  */
-//SIGPWR      30  /* Power failure restart (System V).  */
-//SIGSYS      31  /* Bad system call.  */
-//SIGUNUSED   31
-
-
 void handler(int signum) {
     const char buf[] = "signum=";
-    printf("%s%d\n", buf, signum);
+    printf("%s%d %s\n", buf, signum, sys_siglist[signum]);
     exit(signum);
 }
 
 void main() {
-    printf("pid=%d\n", getpid());
-    signal(SIGSEGV, &handler);
-    signal(SIGTERM, &handler);
-    signal(SIGINT, &handler);
-    int* p = 0x0;
-    int x;
-    //x = *p;
+    printf("pid=%i\n", getpid());
+    struct sigaction sig_action;
+    sigset_t new_set;
+    sigemptyset(&new_set);
+    sigaddset(&new_set, SIGHUP);
+    sigprocmask(SIG_BLOCK, &new_set, 0);
+    sig_action.sa_handler = handler;
+    sigaction(SIGTERM, &sig_action, 0);
     while(1)
         sleep(100);
 }
