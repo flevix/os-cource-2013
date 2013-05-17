73b10dd538cd37471d47a727a4338b8153b7a03d
flevix <flevix.ru@gmail.com>
2013-05-17 23:19:23 +0400

    global variables removed, 2xPrint -> 1xPrint2

--- dupme/main.c
+++ dupme/main.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *data;
-
-void print(int length) {
-    int write_count = 0;
-    while (length > 0) {
-        write_count = write(1, data + write_count, length + 1);
-        if (write_count < 0) exit(1);
-        length -= write_count;
+void print2(int fd, char *buf, int length) {
+    int i;
+    for (i = 0; i < 2; i++) {
+        int write_count = 0;
+        while (length > 0) {
+            write_count = write(fd, buf + write_count, length + 1);
+            if (write_count < 0)
+                exit(4);
+            length -= write_count;
+        }
     }
 }
 
@@ -21,9 +23,9 @@ int main(int argc, char** argv) {
     if (argc != 2)
         exit(1);
     int k = atoi(argv[1]) + 1;
-    if (k == 1)
+    if (k < 2)
         exit(2);
-    data = malloc(k * sizeof(char));
+    char *data = malloc(k * sizeof(char));
     int length = 0;
     int read_count = 0;
     int end_of_file = 0;
@@ -31,16 +33,17 @@ int main(int argc, char** argv) {
     state last_state = NORMAL;
     while (end_of_file == 0) {
         read_count = read(0, data + length, k - length);
-        if (read_count < 0) exit(1);
-        if (read_count == 0) end_of_file = 1;
+        if (read_count < 0)
+            exit(3);
+        if (read_count == 0)
+            end_of_file = 1;
         length += read_count;
         for (i = 0; i < length; i++)
             if (data[i] == '\n') {
                 if (last_state == IGNORING) {
                     last_state = NORMAL;
                 } else if (last_state == NORMAL && i < k && data[0] != '\n') {
-                    print(i);
-                    print(i);
+                    print2(1, data, i);
                 }
                 memmove(data, data + i + 1, k - i - 1);
                 length -= i + 1;
@@ -52,7 +55,7 @@ int main(int argc, char** argv) {
         }
         if (end_of_file && last_state != IGNORING && data[0] != '\n') {
             data[length] = '\n';
-            print(length); print(length);
+            print2(1, data, length);
         }
     }
     free(data);
