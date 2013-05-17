462d9f421b56d88281ace3b652260bbd8f1478bc
flevix <flevix.ru@gmail.com>
2013-05-18 00:35:24 +0400

    gcc -g.. added. Some constant added. Comments added

--- dupme/main.c
+++ dupme/main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+    NORMAL, IGNORING
+} state;
+
 void print2(int fd, char *buf, int length) {
     int i;
     for (i = 0; i < 2; i++) {
@@ -15,46 +19,52 @@ void print2(int fd, char *buf, int length) {
     }
 }
 
-typedef enum {
-    NORMAL, IGNORING
-} state;
-
 int main(int argc, char** argv) {
     if (argc != 2)
         exit(1);
-    int k = atoi(argv[1]) + 1;
-    if (k < 2)
+
+    int k = atoi(argv[1]);
+    if (k < 1)
         exit(2);
-    char *data = malloc(k * sizeof(char));
-    int length = 0;
-    int read_count = 0;
-    int end_of_file = 0;
-    int i = 0;
+    k += 1;
+
+    char *data = (char*) malloc(k * sizeof(char));
+    const char delimiter = '\n';
+    const int fd_read = 0;
+    const int fd_write = 1;
+
     state last_state = NORMAL;
+    int length = 0;
+    int end_of_file = 0;
     while (end_of_file == 0) {
-        read_count = read(0, data + length, k - length);
+        int read_count = read(fd_read, data + length, k - length);
         if (read_count < 0)
             exit(3);
         if (read_count == 0)
             end_of_file = 1;
         length += read_count;
-        for (i = 0; i < length; i++)
-            if (data[i] == '\n') {
+
+        int i;
+        for (i = 0; i < length; i++) {
+            if (data[i] == delimiter) {
                 if (last_state == IGNORING) {
                     last_state = NORMAL;
-                } else if (last_state == NORMAL && i < k && data[0] != '\n') {
-                    print2(1, data, i);
+                } else if (last_state == NORMAL && i < k
+                                && data[0] != delimiter) { //??54
+                    print2(fd_write, data, i);
                 }
-                memmove(data, data + i + 1, k - i - 1);
+                //i think it's need rewrite
+                memmove(data, data + i + 1, (k - i - 1) * sizeof(char)); //??56
                 length -= i + 1;
                 i = 0;
             }
-        if (length == k) {
+        }
+        if (length == k) { //delimiter not found -> ignoring
             last_state = IGNORING;
             length = 0;
         }
-        if (end_of_file && last_state != IGNORING && data[0] != '\n') {
-            data[length] = '\n';
+        if (end_of_file && last_state != IGNORING && data[0] != delimiter) {
+            data[length] = delimiter; //??64 data[0]
             print2(1, data, length);
         }
     }
