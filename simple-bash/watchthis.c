6396d12a6fed582026043e5966ca45e110b0019f
flevix <flevix.ru@gmail.com>
2013-05-19 04:23:38 +0400

    move some code in functions, some variables renamed

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -17,6 +17,10 @@ void error_exit(int code) {
     _exit(code);
 }
 
+void handler() {
+    error_exit(130);
+}
+
 void _rw(char* data, int data_size, int src, int dst) {
     while (1) {
         int read_count = read(src, data, data_size);
@@ -34,8 +38,27 @@ void _rw(char* data, int data_size, int src, int dst) {
     }
 }
 
-void sigint_exit() {
-    error_exit(130);
+int check_creat_files(const char* file1, const char* file2) {
+    int fds[2];
+    fds[0] = open(file1, O_CREAT | O_WRONLY, 0600);
+    fds[1] = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (fds[0] < 0 || fds[1] < 0)
+        return 1;
+    close(fds[0]);
+    close(fds[1]);
+    return 0;
+}
+
+int copy_fail(char* data, int data_size, const char* from, const char* to) {
+    int fds[2];
+    fds[0] = open(from, O_RDONLY);
+    fds[1] = open(to, O_WRONLY | O_TRUNC);
+    if (fds[0] < 0 || fds[1] < 0)
+        return 1;
+    _rw(data, data_size, fds[0], fds[1]);
+    close(fds[0]);
+    close(fds[1]);
+    return 0;
 }
 
 int wait_fail() {
@@ -56,56 +79,43 @@ int main(int argc, char** argv) {
     if (time_for_wait < 0)
         exit(2);
 
-    int fds[2];
-    fds[0] = open(STATE_FIRST, O_CREAT | O_WRONLY, 0600);
-    fds[1] = open(STATE_SECOND, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fds[0] < 0 || fds[1] < 0)
-        error_exit(2);
-    close(fds[0]);
-    close(fds[1]);
-    
-    signal(SIGINT, sigint_exit);
+    const char* st1 = STATE_FIRST;
+    const char* st2 = STATE_SECOND;
+
+    if (check_creat_files(st1, st2))
+       error_exit(2); 
+
+    signal(SIGINT, handler);
     int data_size = 1024;
     char* data = (char*) malloc(data_size * sizeof(char));
+    int fd;
     while (1) {
-
         if (fork()) {
             if (wait_fail())
                 error_exit(3);
-            fds[0] = open(STATE_FIRST, O_RDONLY);
-            if (fds[0] < 0)
+            if ((fd = open(st1, O_RDONLY)) < 0)
                 error_exit(4);
-            _rw(data, data_size, fds[0], STDOUT_FILENO);
-            close(fds[0]);
+            _rw(data, data_size, fd, STDOUT_FILENO);
+            close(fd);
 
             if (fork()) {
                 if (wait_fail())
                     error_exit(5);
             } else {
-                execlp("diff", "diff", "-u", STATE_FIRST, STATE_SECOND, NULL);
+                execlp("diff", "diff", "-u", st1, st2, NULL);
                 exit(255);
             }
 
         } else {
-            fds[1] = open(STATE_FIRST, O_WRONLY);
-            if (fds[1] < 0)
+            if ((fd = open(st1, O_WRONLY)) < 0)
                 error_exit(5);
-
-            dup2(fds[1], STDOUT_FILENO);
-            close(fds[0]);
-            close(fds[1]);
-
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
             execvp(argv[2], &argv[2]);
             exit(255);
         }
-
-        fds[0] = open(STATE_FIRST, O_RDONLY);
-        fds[1] = open(STATE_SECOND, O_WRONLY | O_TRUNC);
-        if (fds[0] < 0 || fds[1] < 0)
+        if (copy_fail(data, data_size, st1, st2))
             error_exit(6);
-        _rw(data, data_size, fds[0], fds[1]);
-        close(fds[0]);
-        close(fds[1]);
         sleep(time_for_wait);
     }
     return 0;
