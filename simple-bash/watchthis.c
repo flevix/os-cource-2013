70bcacbbf15bafea7d1a55d99a8e28ed654b99d4
flevix <flevix.ru@gmail.com>
2013-06-22 01:17:24 +0400

    perror added

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -6,120 +6,159 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
 
-const char *STATE_FIRST = "/tmp/watchthis1.84";
-const char *STATE_SECOND = "/tmp/watchthis2.84";
+const char *STATE_FIRST = "/var/tmp/watchthis1.84";
+const char *STATE_SECOND = "/var/tmp/watchthis2.84";
 
-void error_exit(int code) {
+void safe_exit()
+{
     unlink(STATE_FIRST);
     unlink(STATE_SECOND);
     write(STDOUT_FILENO, "\n", 1);
-    _exit(code);
+    _exit(EXIT_FAILURE);
 }
 
-void handler() {
-    error_exit(130);
+void handler()
+{
+    safe_exit();
 }
 
-void _rw(int src, int dst) {
-    int data_size = 1024;
-    char* data = (char*) malloc(data_size * sizeof(char));
-    if (data == NULL)
-        error_exit(1);
-    while (1) {
-        int read_count = read(src, data, data_size);
-        if (read_count < 0)
-            error_exit(7);
+char* safe_malloc(size_t size)
+{
+    void *ptr = malloc(size);
+    if (ptr == NULL)
+    {
+        perror("Error");
+        safe_exit();
+    }
+    return (char*) ptr;
+}
+
+int safe_read(int fd, char *buf, size_t len)
+{
+    int read_count = read(fd, buf, len);
+    if (read_count == -1)
+    {
+        perror("Error");
+        safe_exit();
+    }
+    return read_count;
+}
+
+void safe_write(int fd, char *buf, size_t len)
+{
+    size_t write_curr = 0;
+    while (write_curr < len)
+    { 
+        int write_count = write(fd, buf + write_curr, len - write_curr);
+        if (write_count == -1)
+        {
+            perror("Error");
+            safe_exit();
+        }
+        write_curr += write_count;
+    }
+}
+
+void read_for_write(int src, int dst)
+{
+    const int data_size = 1024;
+    char *data = safe_malloc(data_size * sizeof(char));
+    while (1)
+    {
+        int read_count = safe_read(src, data, data_size);
         if (read_count == 0)
             break;
-        int pos = 0;
-        while (pos < read_count) {
-            int write_count = write(dst, data + pos, read_count - pos);
-            if (write_count < 0)
-                error_exit(8);
-            pos += write_count;
-        }
+        safe_write(dst, data, read_count);
     }
     free(data);
 }
 
-int check_creat_fail(const char* file1, const char* file2) {
+int safe_open(const char *path, int flags)
+{
+    int fd = open(path, flags);
+    if (fd == -1)
+    {
+        perror("Error");
+        safe_exit();
+    }
+    return fd;
+}
+
+void safe_copy(const char* from, const char* to) {
+    int fds[2];
+    fds[0] = safe_open(from, O_RDONLY);
+    fds[1] = safe_open(to, O_WRONLY | O_TRUNC);
+    read_for_write(fds[0], fds[1]);
+    close(fds[0]);
+    close(fds[1]);
+}
+
+void safe_creat(const char* file1, const char* file2)
+{
     int fds[2];
     fds[0] = open(file1, O_CREAT | O_WRONLY, 0600);
     fds[1] = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fds[0] < 0 || fds[1] < 0)
-        return 1;
+    if (fds[0] == -1 || fds[1] == -1)
+    {
+        perror("Error");
+        exit(EXIT_FAILURE);
+    }
     close(fds[0]);
     close(fds[1]);
-    return 0;
 }
 
-int copy_fail(const char* from, const char* to) {
-    int fds[2];
-    fds[0] = open(from, O_RDONLY);
-    fds[1] = open(to, O_WRONLY | O_TRUNC);
-    if (fds[0] < 0 || fds[1] < 0)
-        return 1;
-    _rw(fds[0], fds[1]);
-    close(fds[0]);
-    close(fds[1]);
-    return 0;
-}
-
-int wait_fail() {
+void safe_wait() {
     int status;
     wait(&status);
     if (!WIFEXITED(status) && WEXITSTATUS(status) != 0)
-        return 1;
-    return 0;
+    {
+        perror("Error");
+        safe_exit();
+    }
 }
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        const char* usage = "Usage: watchthis [time in seconds] [command]";
+        const char* usage = "Usage: watchthis [time in seconds] [command]\n";
         write(STDERR_FILENO, usage, strlen(usage));
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     int time_for_wait = atoi(argv[1]);
     if (time_for_wait < 0)
-        exit(2);
+    {
+        exit(EXIT_FAILURE);
+    }
 
     const char* st1 = STATE_FIRST;
     const char* st2 = STATE_SECOND;
-
-    if (check_creat_fail(st1, st2))
-       error_exit(2); 
+    safe_creat(st1, st2);
 
     signal(SIGINT, handler);
 
     while (1) {
-        int fd;
-        if (fork()) {
-            if (wait_fail())
-                error_exit(3);
-            if ((fd = open(st1, O_RDONLY)) < 0)
-                error_exit(4);
-            _rw(fd, STDOUT_FILENO);
+        if (fork())
+        {
+            safe_wait();
+            int fd = safe_open(st1, O_RDONLY);
+            read_for_write(fd, STDOUT_FILENO);
             close(fd);
 
             if (fork()) {
-                if (wait_fail())
-                    error_exit(5);
+                safe_wait();
             } else {
                 execlp("diff", "diff", "-u", st1, st2, NULL);
                 exit(255);
             }
-
         } else {
-            if ((fd = open(st1, O_WRONLY)) < 0)
-                error_exit(5);
+            int fd = open(st1, O_WRONLY);
             dup2(fd, STDOUT_FILENO);
             close(fd);
             execvp(argv[2], &argv[2]);
             exit(255);
         }
-        if (copy_fail(st1, st2))
-            error_exit(6);
+        safe_copy(st1, st2);
         sleep(time_for_wait);
     }
     return 0;
