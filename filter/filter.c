e6cac65475e880fe432356868c7b4414d7058f89
flevix <flevix.ru@gmail.com>
2013-06-22 02:07:03 +0400

    perror added

--- filter/filter.c
+++ filter/filter.c
@@ -1,16 +1,44 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <string.h>
 
-void print(int fd, char* buf, int length) {
-    int count = 0;
-    while (count < length) {
+char* safe_malloc(size_t size)
+{
+    void *ptr = malloc(size);
+    if (ptr == NULL)
+    {
+        perror("Error");
+        exit(EXIT_FAILURE);
+    }
+    return (char*) ptr;
+}
+
+int safe_read(int fd, char *buf, size_t length)
+{
+    int read_count = read(fd, buf, length);
+    if (read_count == -1)
+    {
+        perror("Error");
+        _exit(EXIT_FAILURE);
+    }
+    return read_count;
+}
+
+void safe_write(int fd, char* buf, size_t length)
+{
+    size_t count = 0;
+    while (count < length)
+    {
         int write_count = write(fd, buf + count, length - count + 1);
-        if (write_count < 0)
-            _exit(4);
+        if (write_count == -1)
+        {
+            perror("Error");
+            _exit(EXIT_FAILURE);
+        }
         count += write_count;
     }
 }
@@ -30,19 +58,22 @@ void _exec(char** _argv, int len, char* data, int length, char delimiter)
     }
     int status;
     wait(&status);
-    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+    {
         data[length] = delimiter;
-        print(STDOUT_FILENO, data, length);
+        safe_write(STDOUT_FILENO, data, length);
     }
 }
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
     int opt;
     int buf_capacity = 4 * 1024;
     char delimiter = '\n';
-    while ((opt = getopt(argc, argv, "nzb:")) != -1) {
-        switch (opt) {
+    while ((opt = getopt(argc, argv, "nzb:")) != -1)
+    {
+        switch (opt)
+        {
             case 'n':
                 delimiter = '\n';
                 break;
@@ -52,7 +83,9 @@ int main(int argc, char** argv) {
             case 'b':
                 buf_capacity = atoi(optarg);
                 if (buf_capacity < 1)
-                   exit(1);
+                {
+                   exit(EXIT_FAILURE);
+                }
                 break;
         }
     }
@@ -61,27 +94,29 @@ int main(int argc, char** argv) {
     int _argv_length = argc - optind + 2;
     char** _argv = (char**) malloc(_argv_length * sizeof(char*));
     if (_argv == NULL)
+    {
         exit(1);
+    }
     memcpy(_argv, argv + optind, (_argv_length - 2) * sizeof(char*));
     _argv[_argv_length - 1] = NULL; //last argv
     
-    char* data = (char*) malloc(buf_capacity * sizeof(char));
-    if (data == NULL)
-        exit(1);
+    char* data = safe_malloc(buf_capacity * sizeof(char));
     int buf_size = 0;
     int eof_flag = 0;
     while (!eof_flag) {
-        int read_count = read(0, data + buf_size, buf_capacity - buf_size);
-        if (read_count < 0)
-            _exit(2);
+        int read_count = safe_read(0, data + buf_size, buf_capacity - buf_size);
         if (read_count == 0)
+        {
             eof_flag = 1;
+        }
         buf_size += read_count;
         
         int first = 0;
         int last;
-        for (last = 0; last < buf_size; last++) {
-            if (data[last] == delimiter) {
+        for (last = 0; last < buf_size; last++)
+        {
+            if (data[last] == delimiter)
+            {
                 _exec(_argv, _argv_length, data + first, last - first, delimiter);
                 first = last + 1;
             }
@@ -90,9 +125,13 @@ int main(int argc, char** argv) {
         buf_size -= first;
 
         if (buf_size == buf_capacity)
-            _exit(3);
+        {
+            _exit(EXIT_FAILURE);
+        }
         if (eof_flag)
+        {
             _exec(_argv, _argv_length, data, buf_size, delimiter);
+        }
     }
     free(data);
     free(_argv);
